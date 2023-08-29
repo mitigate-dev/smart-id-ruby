a4bc634806093b67a11a6135b0eda61a74675a65
5andi5 <5andi5@inbox.lv>
2023-08-29 17:36:20 +0300

    Improvements according to code review (remove commented code)

--- lib/smart_id/api/request.rb
+++ lib/smart_id/api/request.rb
@@ -5,10 +5,6 @@ module SmartId::Api
     DEMO_BASE_URL = "https://sid.demo.sk.ee/smart-id-rp/v1/"
     PRODUCTION_BASE_URL = "https://rp-api.smart-id.com/v1/"
 
-    # DEMO_SSL_KEY = "QLZIaH7Qx9Rjq3gyznQuNsvwMQb7maC5L4SLu/z5qNU="
-    # PROD_KEY_EXPIRY = Date.new(2020,11,5)
-    # PRODUCTION_SSL_KEY = "l2uvq6ftLN4LZ+8Un+71J2vH1BT9wTbtrE5+Fj3Vc5g="
-
     def initialize(method, uri, params)
       @method = method
       @url = self.class.const_get("#{SmartId.environment}_BASE_URL") + uri
@@ -40,15 +36,7 @@ module SmartId::Api
       end
     end
 
-    # def maybe_warn_of_ssl_key_expiry
-    #   if (PROD_KEY_EXPIRY - Date.today).to_i < 60
-    #     @logger.warn("[Smart-id-Ruby] SSL KEY for security checks will soon expire, please update to newer version of this gem")
-    #   end
-    # end
-
     def execute
-      # maybe_warn_of_ssl_key_expiry
-
       if @method.to_sym == :post
         attrs = post_request_attrs
       else
@@ -66,11 +54,6 @@ module SmartId::Api
         url: @url,
         headers: { content_type: :json, accept: :json },
         timeout: SmartId.poller_timeout_seconds + 1,
-        # ssl_verify_callback: lambda do |_, cert_store|
-        #   provided_pub_key = cert_store.chain[0].public_key
-        #   saved_key = self.class.const_get("#{SmartId.environment}_SSL_KEY")
-        #   Digest::SHA256.digest(provided_pub_key.to_der) == Base64.decode64(saved_key)
-        # end
       }
     end
 
